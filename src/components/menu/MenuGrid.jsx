21c39ebecf4567e0bff75cfec4122d9c76733914
weng <ic.erwin.vallez@cvsu.edu.ph>
2025-11-27 16:13:58 +0800

    texts

--- src/components/menu/MenuGrid.jsx
+++ src/components/menu/MenuGrid.jsx
@@ -145,11 +145,11 @@ function MenuGrid({ selectedCategory = "All" }) {
               />
             </div>
 
-            <h3 className="font-semibold text-[#4E342E] text-sm sm:text-base line-clamp-2 mb-1 sm:mb-2 min-h-[36px] sm:min-h-[44px] relative z-10 tracking-wide">
+            <h3 className="font-semibold text-[#4E342E] text-lg sm:text-2xl line-clamp-2 mb-1 sm:mb-2 min-h-[36px] sm:min-h-[44px] relative z-10 tracking-wide">
               {p.name}
             </h3>
 
-            <p className="font-bold text-[#6B3E2E] text-lg sm:text-2xl mb-2 sm:mb-4 relative z-10">
+            <p className="font-bold text-[#6B3E2E] text-base sm:text-xl mb-2 sm:mb-4 relative z-10">
               ₱{p.price}
             </p>
           </div>
